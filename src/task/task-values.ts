1a48bc86a8b6ffdd6f00daa0e20da12f48796861
Matěj Pešl <matejpesl1@gmail.com>
2026-05-04 10:59:52 +0200

    chore: allow gpt-5.3-chat-latest

--- src/task/task-values.ts
+++ src/task/task-values.ts
@@ -31,7 +31,8 @@ function getModels() {
   return [
     "gpt-5.4",
     "gpt-5.4-mini",
-    "gpt-5.5" as "gpt-5.4", // Ugly workaround to satisfy typing - gpt-5.5 is not yet present in the official openapi package, but CAN actually be used.
+    "gpt-5.3-chat-latest",
+    // "gpt-5.5" as "gpt-5.4", // Ugly workaround to satisfy typing - gpt-5.5 is not yet present in the official openapi package, but CAN actually be used.
   ] as const satisfies readonly ExtractLiterals<AllModels>[];
 }
 
